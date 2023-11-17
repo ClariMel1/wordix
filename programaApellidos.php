05c275612326cc683ea43cafb4b648f3fe8ebc44
diegoavila1 <diegoavilaaa0@gmail.com>
2023-11-17 17:59:02 -0300

    actualizacion del switch falta el case(2,4,5,6)

--- programaApellidos.php
+++ programaApellidos.php
@@ -43,42 +43,46 @@ function cargarColeccionPalabras()
 /** PROGRAMA PRINCIPAL **/
 /******/
 
-echo "Ingrese su nombre: ";
-$nombre=trim(fgets(STDIN));
-escribirMensajeBienvenida($nombre);
-do{
-    echo "1- Jugar al wordix con una palabra elegida"."\n";
-    echo "2- Jugar al wordix ocn una palabra aleatoria"."\n";
-    echo "3- Mostrar una partida"."\n";
-    echo "4- Mostrar la primera partida ganadora"."\n";
-    echo "5- Mostrar resumen de jugador"."\n";
-    echo "6- Mostrar listado de partidas ordenadas por jugador y por palabra"."\n";
-    echo "7- Agregar una palabra de 5 letras en wordix"."\n";
-    echo "8- Salir"."\n";
-    echo "Selecciona una opción: ";
-    $respuesta = trim(fgets(STDIN));
-    if ($respuesta == 1){
-    
-    }elseif ($respuesta == 2){
-    
-    }elseif ($respuesta == 3){
-    
-    }elseif ($respuesta == 4){
-    
-    }elseif ($respuesta == 5){
-    
-    }elseif ($respuesta == 6){
-    
-    }elseif ($respuesta == 7){
-        $palabra5Letras=leerPalabra5Letras ();
+do {
+    $opcion = seleccionarOpcion();
+    switch ($opcion) {
+        case 1: 
+            $nombreJugador = solicitarJugador();
+            echo "Ingrese numero de palabra para jugar:";
+            $numero = solicitarNumeroEntre($minimo, $maximo);
+            
+            $coleccPalabras = cargarColeccionPalabras();
+            $palabraWordix = $coleccPalabras [$numero];
 
-    }elseif ($respuesta == 8){
-        
-    }else{
-        echo "Ingrese una opción valida: ";
+            $partida = jugarWordix($palabraWordix, $nombreJugador);
+            print_r ($partida);
+
+            break;
+        case 2: 
+            "ingrese su nombre:";
+            $nombre =trim(fgets(STDIN));
+            $variable=palabraAleatoria($coleccionPalabras,$nombre);
+
+            break;
+        case 3: 
+            echo "Ingrese nro de partida:";
+            $nroPartida = trim(fgets(STDIN));
+            $partida = mostrarDatosPartida($nroPartida, $datosPartidas);
+            break;
+        case 4: 
+            break;
+        case 5: 
+            break;
+        case 6:
+            break;
+        case 7:
+            $palabraNueva = leerPalabra5Letras();
+            $coleccPalabras = cargarColeccionPalabras();
+            $coleccionModificada = agregarPalabra($coleccionPalabras, $palabra);
+            print_r ($coleccionModificada);
+            break;
     }
-    
-}while($respuesta != 8);
+} while ($opcion != 8);
 
 //Declaración de variables:
 
@@ -94,26 +98,3 @@ do{
 
 
 
-/*
-do {
- //   $opcion = ...;
-
-    
-    switch ($opcion) {
-        case 1: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
-
-            break;
-        case 2: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
-
-            break;
-        case 3: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
-
-            break;
-        
-            //...
-    }
-} while ($opcion != X);
-//*/
